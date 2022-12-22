fae71bb3021a22a865e50bfb0f1f7ec662c1ec00
krishnav89 <krish.swamy007@gmail.com>
2022-12-22 18:34:59 +0530

    testing jenkins pipeline

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! from Docker Application" );
+        System.out.println( "Hello World! This application is deployed via Jenkins pipeline to docker hub" );
     }
 }
